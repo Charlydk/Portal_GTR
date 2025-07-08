3ac0070d44583573d7a182a1f489c64364b88f3c
Fabian Bernardino <fabian.bernardino@gmail.com>
2025-07-08 10:16:50 -0300

    definiendo estructura de la pagina

---
+++ frontend/portal-gtr-frontend/src/pages/AnalistasPage.jsx
@@ -0,0 +1,13 @@
+// src/pages/AnalistasPage.jsx
+import React from 'react';
+
+function AnalistasPage() {
+  return (
+    <div className="container mt-4">
+      <h2>Vista de Analistas</h2>
+      <p>Aquí se gestionarán los datos de los analistas.</p>
+    </div>
+  );
+}
+
+export default AnalistasPage;
---
+++ frontend/portal-gtr-frontend/src/pages/CampanasPage.jsx
@@ -0,0 +1,13 @@
+// src/pages/CampanasPage.jsx
+import React from 'react';
+
+function CampanasPage() {
+  return (
+    <div className="container mt-4">
+      <h2>Vista de Campañas (Calendario)</h2>
+      <p>Aquí se mostrará el calendario y la gestión de campañas.</p>
+    </div>
+  );
+}
+
+export default CampanasPage;
